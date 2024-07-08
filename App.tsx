cb53be8b5c6b1c87a0c0c9e3cf3c88762dc05c0e
Muhammad Asif <145665646+muhammadasifdotai@users.noreply.github.com>
2024-07-08 13:55:48 +0500

    code for two languages

--- App.tsx
+++ App.tsx
@@ -14,24 +14,39 @@ import languagesList from './services/languagesList.json';
 
 const App = () => {
   const [visible, setVisible] = useState(false);
+  // t: to use this 't' method to get the values for the Translation
   const {t} = useTranslation();
 
+  // here in the 'changeLng' function we will get the language'lng' and for changing language we use 'i18next' that we imported and there is a method '.changeLanguage' and we change the language to this 'lng', that we got here as an argument 
+  // .. and when we change the language we need to set the visible to false 'setVisible(false)' so modal will be disappeared
   const changeLng = lng => {
     i18next.changeLanguage(lng);
     setVisible(false);
   };
 
+  // {/* user translate between different languages for this we used modal */}
+  // visible={visible}: modal is visible when the state is true, 
+  // when the button is clicked the modal is visible so we need to edit below onPress, i.e is in button 'change-language', in this we set the visible true
   return (
     <SafeAreaView style={styles.container}>
+      {/* we also 'onRequestClose' we want to set the visible false like that 'setVisible(false)' */}
       <Modal visible={visible} onRequestClose={() => setVisible(false)}>
         <View style={styles.languagesList}>
           <FlatList
+        //  data={Object.keys(languageResources)} : for the data we will use object dot keys and we will extract the keys of language resource (the keys are english and swedish)
+        // ... languageResources: we imported this from i18next.
+        // .. then we will use these keys to extrack the name of this languages (languages that are present in 'languagesList.json') 
+        // .. so for the renderItem we will render the touchableOpacity, in which we have a text, so the text will be the name of the language and to access the name of the language, we can get the language list,
+        // .. so first we need to import the 'languagesList' from 'services'
+        // .. so now we can use those keys to access the name of the language
             data={Object.keys(languageResources)}
             renderItem={({item}) => (
               <TouchableOpacity
                 style={styles.languageButton}
+                // for change the language we can add here onPress in the button and we went to change the language to the item 'changeLng(item)'
                 onPress={() => changeLng(item)}>
                 <Text style={styles.lngName}>
+                  {/* // so here when we imported the languagesList we can then access the item which will be a key and then when we selected a language we can get the native name of it so we go back to 'languagesList' we have a name and nativeName and we will use this native name  */}
                   {languagesList[item].nativeName}
                 </Text>
               </TouchableOpacity>
--- services/i18next.js
+++ services/i18next.js
@@ -3,11 +3,23 @@ import {initReactI18next} from 'react-i18next';
 import en from '../locales/en.json';
 import sv from '../locales/sv.json';
 
+// languageResources: is an object, it will have different different languages, in our case is englis: which contain an object which have 'translation' and then we will pass the translation file or the translation json that we imported 
+// ... and same for the swedish
+// we create separate variable for this because we need this in 'App.tsx'
 export const languageResources = {
+  // here we have a list of two keys 'en: english' and 'sv: swedish'
   en: {translation: en},
   sv: {translation: sv},
 };
 
+// In this line we are using '.use' method provided by 'i18next' to add the 'initReactI18next' to our localization setup
+// .. 'initReactI18next' is a plugin specifically design for integrationg i18next with the react native applications
+// .. 'initReactI18next' it basically provide react basically features and hooks that make it easier to handle translation with the react components
+// .. then type 'init' to initialize the 'initReactI18next' and then we pass 'object' in this init method
+// .. then first key this object have is 'compatibilityJSON: 'v3',' with version '3',
+// ... 'lng: 'en',': lng for language and the default language is english.
+// ...fallbackLng: 'en',:  for example , iger app may ksi language ki key nhi hay e.g '"change-language": "Ändra språket",' ya key or value ka pair hay. to app fallback language may chali jayee gi.
+// .. 'resources:' : will containe the available languages and their corresponding translation and actually for this one you will create another variable
 i18next.use(initReactI18next).init({
   compatibilityJSON: 'v3',
   lng: 'en',
